ca4a000a06c33e53fc0c3fc068f9530fc28484f9
Sreenivas Y <svreddyy@gmail.com>
2021-07-26 16:04:03 +0530

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => 'root',
+		'username' => 'testuser',
+		'password' => 'Fz(}TsZw$XQWH33',
 		'database' => 'wise1',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
